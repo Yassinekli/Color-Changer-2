365fbe0e3cac4f28862e3934f15753438b6c55de
Yassine KLILICH <klilichyassine8@gmail.com>
2018-02-10 22:48:42 +0000

    Color Change 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Color_Controler = new System.Windows.Forms.TrackBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Color_Controler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Color_Controler
+            // 
+            this.Color_Controler.Location = new System.Drawing.Point(12, 276);
+            this.Color_Controler.Maximum = 1530;
+            this.Color_Controler.Name = "Color_Controler";
+            this.Color_Controler.Size = new System.Drawing.Size(471, 45);
+            this.Color_Controler.TabIndex = 0;
+            this.Color_Controler.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.Color_Controler.Scroll += new System.EventHandler(this.Color_Controler_Scroll);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(471, 246);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 316);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Color_Controler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Color_Controler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar Color_Controler;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int A = 255, R = 255, G = 0, B = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Color_Controler.Value = 255;
+            Color color = Color.FromArgb(A, R, G, B);
+            richTextBox1.BackColor = color;
+        }
+
+        private void Color_Controler_Scroll(object sender, EventArgs e)
+        {
+            if (Color_Controler.Value < 255)
+            {
+                R = Color_Controler.Value;
+                G = 0;
+                B = 0;
+                
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+
+            if (Color_Controler.Value > 255 && Color_Controler.Value < 510)
+            {
+                R = 255;
+                G = Color_Controler.Value - 255;
+                B = 0;
+                
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+
+            if (Color_Controler.Value > 510 && Color_Controler.Value < 765 )
+            {
+                R = 765-Color_Controler.Value;
+                //Condition****************************
+                if(G > 126)
+                G = 764 - Color_Controler.Value;
+
+                if (G == 0)
+                    G = 125;
+
+                B = 0;
+                
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+
+            if (Color_Controler.Value > 765 && Color_Controler.Value < 1020)
+            {
+                R = 0;
+
+                if (Color_Controler.Value > 765 && Color_Controler.Value < 892)
+                    G = 893 - Color_Controler.Value;
+                
+                B = Color_Controler.Value - 765;
+
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+
+            if (Color_Controler.Value > 1020 && Color_Controler.Value < 1275)
+            {
+                R = Color_Controler.Value - 1020;
+                G = 0;
+                B = 255;
+
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+            if (Color_Controler.Value > 1275 && Color_Controler.Value < 1530)
+            {
+                R = 255;
+                G = Color_Controler.Value - 1275;
+                B = 255;
+
+                Color color = Color.FromArgb(A, R, G, B);
+                richTextBox1.BackColor = color;
+            }
+        }
+
+    }
+}
